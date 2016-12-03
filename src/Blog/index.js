a9e4f252f214cf4fd186f11140a21fe280edef6b
youngjingwen <798806504@qq.com>
2016-12-03 11:08:08 +0800

    blog

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,10 +1,20 @@
 import React, { PropTypes } from 'react'
+import BlogCard from '../component/BlogCard'
+
+let data =[
+  {index:1,title:'hello',desc:'world'},
+  {index:2,title:'hello2',desc:'world2'},
+  {index:3,title:'hello3',desc:'world3'},
+  {index:4,title:'hello4',desc:'world4'},
+  {index:5,title:'hello5',desc:'world5'},
+]
 
 class Blog extends React.Component {
   render () {
+    let cards = data.map((item,i) => <BlogCard {...item} key={i}/>)
     return(
-      <div>
-        Blog页面
+      <div className='blog-wrap'>
+      {cards}
       </div>
     )
   }
---
+++ src/component/BlogCard.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+
+class BlogCard extends React.Component {
+  render () {
+    return(
+      <div className='blog-card'>
+        <div className='blog-index'><span>{this.props.index}</span></div>
+        <div className='blog-content clearfix'>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.desc}</p>
+          <RaisedButton label='阅读更多' paimary={true} />
+        </div>
+      </div>
+    )
+  }
+}
+
+BlogCard.defaultProps={
+  index:1,
+  title:'我是默认标题',
+  desc:'我是默认介绍',
+}
+BlogCard.propTypes={
+  index: PropTypes.number,
+  title: PropTypes.string,
+  desc: PropTypes.string,
+}
+export default BlogCard;
